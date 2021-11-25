66313b712d6552701c0a74c6f6efcf42c0bad9f4
asullyy <karen.silva@icen.ufpa.br>
2021-11-25 19:57:44 -0300

    Commit

---
+++ Personagem.h
@@ -0,0 +1,20 @@
+#ifndef Personagem_H
+#define Personagem_H
+
+#include <iostream>
+
+using std::cout;
+using std::cout;
+
+class Personagem
+{
+public:
+    void mostrarMensagem() const
+    {
+        cout << "Hello word!\n";
+
+    }
+
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using std::cout;
+
+#include "Personagem.h"
+
+//This is where the execution of program begins
+int main()
+{
+   
+   Personagem pers1;
+   Personagem pers2;
+
+   pers1.mostrarMensagem();
+   pers2.mostrarMensagem();
+   
+   return 0;
+}
